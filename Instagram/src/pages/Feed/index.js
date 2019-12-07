25dda8088e9c81232eba4ee9141b6c6afad705c4
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-07 14:21:03 -0300

    Animations, lazyImage and final ajustments

--- Instagram/src/pages/Feed/index.js
+++ Instagram/src/pages/Feed/index.js
@@ -1,38 +1,77 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, PostImage, Description, Name} from './styles';
+import LazyImage from '../../components/LazyImage';
+
+import {Post, Header, Avatar, PostImage, Description, Name, Loading} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [viewable, setViewable] = useState([]);
+
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if (total && pageNumber > total) return;
+
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3333/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed(shouldRefresh ? data : [...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        `http://localhost:3000/feed?_expand=author&_limit=5&_page=1`,
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
+
+  const handleViewableChanged = useCallback(({ changed}) => {
+    setViewable(changed.map(({ item }) => item.id));
+  }, []);
+
 
   return (
     <View>
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
-        renderitem={({item}) => (
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        onViewableItemsChanged={handleViewableChanged}
+        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 20 }}
+        ListFooterComponent={loading && <Loading/>}
+        renderItem={({item}) => (
           <Post>
             <Header>
               <Avatar source={{uri: item.author.avatar}} />
               <Name>{item.author.name}</Name>
             </Header>
 
-            <PostImage ratio={item.aspect} source={{uri: item.image}} />
+            <LazyImage 
+              shouldLoad={viewable.includes(item.id)}
+              aspectRatio={item.aspectRatio} 
+              smallSource={{uri: item.small}}
+              source={{uri: item.image}} />
 
             <Description>
               <Name>{item.author.name}</Name> {item.description}
