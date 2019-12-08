9f26b373ddd99c04ee5bd38f0f351abbbc699d92
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-08 17:58:14 -0300

    Initial app - Map, Search and start directions

---
+++ uber/src/components/Directions/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MapViewDirections from 'react-native-maps-directions';
+
+const Directions = ({ destination, origin, onReady}) => (
+  <MapViewDirections
+    destination={destination}
+    origin={origin}
+    onReady={onReady}
+    apikey={AIzaSyC5-gI3RY5tfsW2l1_4SPpBWCTnTYpoq6c}
+    strokeWidth={3}
+    strokeColor="#222"
+  />
+  );
+
+export default Directions;
