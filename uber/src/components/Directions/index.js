8213c1141f2e845c28b3a8fa23c3d252cc1c270e
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-11 14:01:45 -0300

    App - To fix: directions

--- uber/src/components/Directions/index.js
+++ uber/src/components/Directions/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import MapViewDirections from 'react-native-maps-directions';
+import MapViewDirections from "react-native-maps-directions";
+import getDirections from 'react-native-google-maps-directions'
 
-const Directions = ({ destination, origin, onReady}) => (
-  <MapViewDirections
-    destination={destination}
-    origin={origin}
-    onReady={onReady}
-    apikey={AIzaSyC5-gI3RY5tfsW2l1_4SPpBWCTnTYpoq6c}
-    strokeWidth={3}
-    strokeColor="#222"
-  />
+const Directions = ({ destination, origin, onReady }) => (
+    <MapViewDirections
+      destination={destination}
+      origin={origin}
+      onReady={onReady}
+      apikey="AIzaSyC5-gI3RY5tfsW2l1_4SPpBWCTnTYpoq6c"
+      strokeWidth={3}
+      strokeColor="#222"
+    />
   );
 
 export default Directions;
--- uber/src/components/Map/index.js
+++ uber/src/components/Map/index.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import MapView from 'react-native-maps';
+import {View, Image} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import Geocoder from 'react-native-geocoding';
+
+import { getPixelSize } from '../../utils';
 
 import Search from '../Search';
 import Directions from '../Directions';
+import Details from '../Details';
+
+import markerImage from '../../assets/marker.png'
+import backImage from '../../assets/back.png'
+
+import {LocationBox, LocationText, LocationTimeBox, LocationTimeText, LocationTimeTextSmall, Back} from './styles';
+
+Geocoder.init("AIzaSyC5-gI3RY5tfsW2l1_4SPpBWCTnTYpoq6c");
 
 export default class Map extends Component {
   state = {
     region: null,
     destination: null,
+    duration: null,
+    location: null,
   };
 
   async componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-       ({ coords: {latitude, longitude} }) => {
+      async ({ coords: { latitude, longitude } }) => {
+        const response = await Geocoder.from({ latitude, longitude });
+        const address = response.results[0].formatted_address;
+        const location = address.substring(0, address.indexOf(','));
+
+        console.log(address);
+
         this.setState({
+          location,
           region: {
             latitude,
             longitude,
@@ -28,7 +48,7 @@ export default class Map extends Component {
         timeout: 2000,
         enableHighAccuracy: true,
         maximumAge: 1000
-      },
+      }
     );
   }
 
@@ -46,34 +66,81 @@ handleLocationSelected = (data, { geometry }) => {
     });
   };
 
+  handleBack = () => {
+    this.setState({ destination: null });
+  };
+
   render() {
-    const {region, destination} = this.state;
+    const { region, destination, duration, location } = this.state;
 
     return (
       <View style={{ flex: 1 }}>
         <MapView
           style={{ flex: 1 }}
-          region={region
+          region={region}
           //   {
           //   latitude: -25.0889004,
           //   longitude: -54.2549483,
           //   latitudeDelta: 0.0143,
-          //   longitudeDelta: 0.0134,
+          //   longitudeDelta: 0.0134
           // }
-        }
           showsUserLocation
           loadingEnabled
+          ref={el => (this.mapView = el)}
         >
-          { destination && (
-            <Directions
-              origin={region}
-              destination={destination}
-              onReady={() => {}}
-            />
+          {destination && (
+            <>
+              <Directions
+                origin={region}
+                destination={destination}
+                onReady={result => {
+                  this.setState({ duration: Math.floor(result.duration) })
+                  this.mapView.fitToCoordinates(result.coordinates, {
+                    edgePadding: {
+                      right: getPixelSize(50),
+                      left: getPixelSize(50),
+                      top: getPixelSize(50),
+                      bottom: getPixelSize(350)
+                    }
+                  });
+                }}
+              />
+              <Marker 
+                coordinate={destination} 
+                anchor={{ x: 0, y: 0 }} 
+                image={markerImage}
+                >
+                  <LocationBox>
+                    <LocationText>{destination.title}</LocationText>
+                  </LocationBox>
+                </Marker>
+
+                <Marker 
+                  coordinate={region} 
+                  anchor={{ x: 0, y: 0 }} 
+                >
+                  <LocationBox>
+                    <LocationTimeBox>
+                      <LocationTimeText>{duration}</LocationTimeText>
+                      <LocationTimeTextSmall>MIN</LocationTimeTextSmall>
+                    </LocationTimeBox>
+                    <LocationText>{location}</LocationText>
+                  </LocationBox>
+                </Marker>
+            </>
           )}
         </MapView>
-
-        <Search onLocationSelected={this.handleLocationSelected} />
+        
+        {destination ? (
+          <>
+            <Back onPress={this.handleBack}>
+              <Image source={backImage} />
+            </Back>
+            <Details /> 
+          </>
+          ) : (
+          <Search onLocationSelected={this.handleLocationSelected} /> 
+          )}
       </View>
     );
   }
