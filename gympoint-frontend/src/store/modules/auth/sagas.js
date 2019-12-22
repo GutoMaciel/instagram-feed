a58d75ee25a9955134af093f828e3af642d3447a
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-22 10:04:33 -0300

    Feature: Auth Toasts

--- gympoint-frontend/src/store/modules/auth/sagas.js
+++ gympoint-frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import { signInSuccess, signFailure } from './actions';
 
@@ -20,6 +21,7 @@ export function* signIn({ payload }) {
 
     history.push('/students/list');
   } catch (err) {
+    toast.error('Auth Fail. Verifie your credentials.');
     yield put(signFailure());
   }
 }
