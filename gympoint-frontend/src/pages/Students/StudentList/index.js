a966e2dd0ea9e1a79c3620537b42fc664bbb3583
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-22 10:15:54 -0300

    Feature: Auth Request

--- gympoint-frontend/src/pages/Students/StudentList/index.js
+++ gympoint-frontend/src/pages/Students/StudentList/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function StudentList() {
-  return (
-    <h1>Students</h1>
-  );
-}
+  api.get('students');
 
+  return <h1>Students</h1>;
+}
